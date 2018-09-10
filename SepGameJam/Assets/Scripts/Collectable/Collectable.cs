03479e5dd6a9fc188ae18e4489f21abd32f70d09
20001LastOrder <boqi.chen@mail.mcgill.ca>
2018-09-09 22:18:16 -0400

    Create Collectable script, start working on managers

---
+++ SepGameJam/Assets/Scripts/Collectable/Collectable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    [SerializeField]
+    private Color _color;
+
+    private string _playerTagName = "Player";
+
+    public float points;
+
+    private void Start() {
+		var material = gameObject.GetComponent<Renderer>().material;
+		if(material != null) {
+			material.color = _color;
+		}
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag.Equals(_playerTagName)) {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
