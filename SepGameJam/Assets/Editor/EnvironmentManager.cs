bf574ec6a030f1d1e9fa71ecd0276c66472ece49
Lancer <gjtqiyue@gmail.com>
2018-09-13 12:18:25 -0400

    start building levels

--- SepGameJam/Assets/Editor/EnvironmentManager.cs
+++ SepGameJam/Assets/Editor/EnvironmentManager.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
 
-public class EnvironmentManager : MonoBehaviour {
+public class EnvironmentManager : Editor {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
 }
